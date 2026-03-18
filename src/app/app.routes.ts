79c9087e0e78b85a930343c9e3880c741694b146
Patrick <bevis.patrick@gmail.com>
2026-03-18 15:20:10 +0100

    feat: notFound page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { NotFound } from './pages/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"", redirectTo:"home", pathMatch:"full"},
+    {path:"home", component:Home},
+    {path:"**", component:NotFound}
+];
